73e02e031030340e6875dbebc3d07c26c71a7966
mih1tm <143355207+mih1tm@users.noreply.github.com>
2024-10-21 16:51:31 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const set_intrebari = {
+    name: "set intrebari",
+    counter: 0,
+    array_seturi: [],
+    
+    create_set: function(question, response1, response2){
+        question = document.getElementById('question_1')
+        response1 = document.getElementById('response_1')
+        response2 = document.getElementById('response_2')
+    }
+
+}
+
+const add_seturi = {
+    verificare_lungime_input: function(){
+
+    },
+    adaugare_id_unic: function(){
+
+    },
+    verificare_input_finish: function(){
+
+    }
+}
+
+const salvare_info = {
+    salvare_info_localstorage: function(){
+
+    }
+}
+
+const printare_istoric = {
+    preluare_info_localstorage: function(){
+
+    },
+    printeaza_istoric: function(){
+
+    }
+
+}
